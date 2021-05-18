f9e71f00947da09b87bafc3e36c0b58914870957
Crowdbotics <team@crowdbotics.com>
2021-05-18 01:04:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/jobcrate_26835/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jobcrate_26835;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "jobcrate_26835";
+  }
+}
